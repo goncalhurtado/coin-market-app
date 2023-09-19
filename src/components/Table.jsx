be42552b46a13d902ce68289a2e4b0794550190c
goncalhurtado <gonzalomilano@gmail.com>
2023-09-18 22:42:03 -0300

    added all data and rows information for crypto table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -18,16 +18,30 @@ const Table = ({ data }) => {
           <p className="m-0 ms-1">{row.symbol}</p>
         </div>
       ),
+    },
+    {
+      name: "Precio",
+      selector: (row) => row.current_price,
       sortable: true,
     },
     {
-      name: "Director",
-      selector: (row) => row.director,
+      name: "24 h %",
+      selector: (row) => row.price_change_percentage_24h.toFixed(3),
       sortable: true,
     },
     {
-      name: "Year",
-      selector: (row) => row.year,
+      name: "Cap. del mercado",
+      selector: (row) => row.market_cap,
+      sortable: true,
+    },
+    {
+      name: "Volumen (24h)",
+      selector: (row) => row.total_volume,
+      sortable: true,
+    },
+    {
+      name: "Acciones en circulacion",
+      selector: (row) => row.circulating_supply,
       sortable: true,
     },
   ];
